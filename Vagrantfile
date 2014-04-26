de7930dd913aa00cb609fd042a4e812a539729fb
Chris <ctarwater@madsecurity.com>
2014-04-25 20:43:00 -0700

    replace open-vm-tools with vmware-tools

--- Vagrantfile
+++ Vagrantfile
@@ -6,12 +6,11 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure("2") do |config|
   config.vm.box = "cmad/kali" #download box from vagrant cloud: org/boxname
-
     config.vm.provider "virtualbox" do |vb|
     vb.gui = true   
   end
-    config.vm.provider "vmware_fusion" do |v|
-    v.gui = true
+    config.vm.provider "vmware_fusion" do |vmware|
+    vmware.gui = true
   end
 
 end
--- scripts/vmware-tools.sh
+++ scripts/vmware-tools.sh
@@ -1,33 +1,11 @@
-apt-get -y install open-vm-toolbox
+# Install open-vm-tools
+#apt-get -y install open-vm-toolbox
 
-## These steps/patches are provided by the Kali team @ https://github.com/offensive-security/kali-vmware-tools-patches
-#apt-get install linux-headers-$(uname -r)
-#sed -i '/cups\ disabled/d' /usr/sbin/update-rc.d 
-#echo cups enabled >> /usr/sbin/update-rc.d 
-#echo vmware-tools-thinprint enabled >> /usr/sbin/update-rc.d
-
-## Configure Git before we do the next steps
-#git config --global user.name
-#git config --global user.email
-
-## Grab the vmware-tools patches
-#git clone https://github.com/offensive-security/vmware-tools-patches
-
-## Grab 
-#cd vmware-tools-patches
-#cp VMwareTools-9.6.1-1378637.tar.gz vmware-tools-patches/
-
-## Now we will apply the patches and build vmware-tools
-#cd vmware-tools-patches
-#export VMWARE_TOOLS_PATCHES_DEBUG=1
-#./untar-all-and-patch.sh
-#cd vmware-tools-distrib
-#sed -i "s/'RUN_CONFIGURATOR', 'yesno', 'yes');/, 'RUN_CONFIGURATOR', 'yesno', 'no');/" vmware-install.pl 
-#./vmware-install.pl -d
-#cd ..
-#patch -p0 < patches/vmware-tools.patch 
-#vmware-config-tools.pl -d
-
-## Now lets remove the Git remnants
-#rm ~/.gitconfig
-#rm -rf ~/vmware-tools-patches
+# Install vmware-tools
+cd /tmp
+mkdir -p /mnt/cdrom
+mount -o loop /home/vagrant/linux.iso /mnt/cdrom
+tar zxvf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
+/tmp/vmware-tools-distrib/vmware-install.pl -d
+rm /home/vagrant/linux.iso
+umount /mnt/cdrom
