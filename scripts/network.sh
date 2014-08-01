ff099ee88a2d93a7419b7824cf5c14a2f9fe2066
Chris <ctarwater@madsecurity.com>
2014-08-01 12:36:34 -0700

    fix networking for vbox and vmware both

--- scripts/network.sh
+++ scripts/network.sh
@@ -17,12 +17,6 @@ EOL
 
 # Test for Virtualbox
 elif test -f .vbox_version ; then
-cat >>/etc/network/interfaces <<EOL
-auto eth1
-iface eth1 inet static
-	address 172.16.189.5
-	netmask 255.255.0.0
-        hwaddress ether 08:00:27:f3:4a:5d
-EOL
+echo "Nothing to see here, move along"
 
 fi
